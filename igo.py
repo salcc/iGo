2aa2a4244199351361750397d11037751c4f56e4
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2021-04-25 13:40:45 +0200

    Refactor functions to save and load data, and to check if a file exists. Use cache for highways file. Updated code comments.

--- igo.py
+++ igo.py
@@ -9,6 +9,7 @@ import staticmap
 PLACE = 'Barcelona, Catalonia'
 GRAPH_FILENAME = 'barcelona.graph'
 SIZE = 800
+HIGHWAYS_FILENAME = 'highways.csv'
 HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-1c40-4609-8620-14ad49aae3ab/resource/1d6c814c-70ef-4147-aa16-a49ddb952f72/download/transit_relacio_trams.csv'
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
@@ -17,25 +18,24 @@ Highway = collections.namedtuple('Highway', 'description coordinates')
 Congestion = collections.namedtuple('Congestion', 'datetime current_state planned_state')
 
 
-def exists_graph(GRAPH_FILENAME):
-    return os.path.isfile(GRAPH_FILENAME)
+def file_exists(filename):
+    return os.path.isfile(filename)
+
+
+def save_data(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+
+
+def load_data(filename):
+    with open(filename, 'rb') as file:
+        data = pickle.load(file)
+        return data
 
 
 def download_graph(PLACE):
     graph = osmnx.graph_from_place(PLACE, network_type='drive', simplify=True)
     graph = osmnx.utils_graph.get_digraph(graph, weight='length')
-    return graph
-
-
-def save_graph(graph, GRAPH_FILENAME):
-    with open(GRAPH_FILENAME, 'wb') as file:
-        pickle.dump(graph, file)
-    return graph
-
-
-def load_graph(GRAPH_FILENAME):
-    with open(GRAPH_FILENAME, 'rb') as file:
-        graph = pickle.load(file)
     return graph
 
 
@@ -99,11 +99,10 @@ def plot_congestions(highways, congestions, output_filename, SIZE):
             highway_line = staticmap.Line((highway.coordinates), (255, 0, 0), 2)
             map.add_line(highway_line)
         if congestion == 6:
-            highway_line = staticmap.Line((highway.coordinates), (64, 4, 4) , 2)
+            highway_line = staticmap.Line((highway.coordinates), (64, 4, 4), 2)
             map.add_line(highway_line)
     map_image = map.render()
     map_image.save(output_filename)
-
 
 
 def build_igraph(graph, highways, congestions):
@@ -135,26 +134,33 @@ def plot_path(ipath, output_filename, SIZE):
 
 
 def test():
-    # load/download graph (using cache) and plot it on the screen
-    if not exists_graph(GRAPH_FILENAME):
-        graph = download_graph(PLACE)
-        save_graph(graph, GRAPH_FILENAME)
+    # load the graph, or download it if it does not exist
+    if file_exists(GRAPH_FILENAME):
+        graph = load_data(GRAPH_FILENAME)
     else:
-        graph = load_graph(GRAPH_FILENAME)
+        graph = download_graph(PLACE)
+        save_data(graph, GRAPH_FILENAME)
 
     # download highways and plot them into a PNG image
-    highways = download_highways(HIGHWAYS_URL)
+    if file_exists(HIGHWAYS_FILENAME):
+        highways = load_data(HIGHWAYS_URL)
+    else:
+        highways = download_highways(HIGHWAYS_URL)
+        save_data(highways, HIGHWAYS_FILENAME)
+    # plot the highways into a PNG image
     plot_highways(highways, 'highways.png', SIZE)
 
-    # download congestions and plot them into a PNG image
+    # download congestions (we download them every time because they are updated every 5 minutes)
     congestions = download_congestions(CONGESTIONS_URL)
+    # plot the congestions into a PNG image
     plot_congestions(highways, congestions, 'congestions.png', SIZE)
 
     # get the 'intelligent graph' version of a graph taking into account the congestions of the highways
     igraph = build_igraph(graph, highways, congestions)
 
-    # get 'intelligent path' between two addresses and plot it into a PNG image
+    # get 'intelligent path' between two addresses
     ipath = get_shortest_path_with_itimes(igraph, "Campus Nord", "Sagrada Família", PLACE)
+    # plot the path into a PNG image
     plot_path(ipath, 'path.png', SIZE)
 
 
